9d0109d35724133e7703f89efbe0aff15fdc26cf
sanny085 <sannykumar085.com>
2021-09-29 10:12:56 +0530

    update package

---
+++ deskStructure.js
@@ -0,0 +1,15 @@
+// ./src/deskStructure.js
+import SeoPane from 'sanity-plugin-seo-pane'
+
+// ...all other list items
+
+S.view
+  .component(SeoPane)
+  .options({
+    keywords: `seo.keywords`,
+    synonyms: `seo.synonyms`,
+    url: (doc) => resolveProductionUrl(doc),
+  })
+  .title('SEO')
+
+  
